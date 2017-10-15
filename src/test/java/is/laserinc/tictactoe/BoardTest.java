a6625d20216ab355d4de86ee39b6bd54bec9d6d0
Ragnarstefanss <ragnars15@ru.is>
2017-10-15 23:24:17 +0000

    Created test and a basic template for BoardTest.java

---
+++ src/test/java/is/laserinc/tictactoe/BoardTest.java
@@ -0,0 +1,15 @@
+package is.laserinc.tictactoe;
+
+public class BoardTest {
+    @Test
+    public void switchPlayerTest(){
+      // create test for this
+    }
+
+    @Test
+    public void checkWinnerRow(){
+      // create test for this
+    }
+
+    // ADD MANY MORE TEST HERE
+}
