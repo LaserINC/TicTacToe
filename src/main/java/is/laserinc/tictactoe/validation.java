16eb88079c9fe88ae23d00a3a481898d9021908f
Hafdis <hafdissae13@ru.is>
2017-10-16 20:39:45 +0000

    Name verification and test

--- src/main/java/is/laserinc/tictactoe/validation.java
+++ src/main/java/is/laserinc/tictactoe/validation.java
@@ -1,17 +1,43 @@
 bool verification(string name){
- 
-	if(name.length() > 12 || name.length() < 1){
-		
-		throw new RunTimeException("Name length invalid!");
-
-	}
-
+	
 	Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
 	Matcher m = p.matcher(name);
 	boolean b = m.find();
+ 
+	if(name.length() > 12 || name.length() < 1){
+		
+		return false;
+	}
 
-	if (b){
+	else if (b){
 
-		throw new RunTimeException("No special characters!");
+		return false;
+	}
+
+	else{
+		return true;
 	}
 }
+
+@Test
+	public void nameOk(){
+		assertEquals(TRUE, TicTacToe.verification("H4FD15"));
+	}
+
+@Test
+	public void specialChar(){
+		assertEquals(FALSE, TicTacToe.verification("H4fd1?"));
+	}
+
+@Test
+	public void nameToLong(){
+		assertEquals(FALSE, TicTacToe.verification("H4fd15543L4ND1337N3RD"));
+	}
+
+@Test
+	public void emptyName(){
+		assertEquals(FALSE, TicTacToe.verification(""));
+	}
+
+
+
