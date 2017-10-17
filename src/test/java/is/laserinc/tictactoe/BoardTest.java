ccae4e572d8aeb52325a150e846463fc402dc7be
Ragnarstefanss <ragnars15@ru.is>
2017-10-17 20:00:51 +0000

    Made boardtest beutifull

--- src/test/java/is/laserinc/tictactoe/BoardTest.java
+++ src/test/java/is/laserinc/tictactoe/BoardTest.java
@@ -4,59 +4,60 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
-    @Test
-    public void switchPlayerTest(){
-      // create test for this
-    }
 
-    @Test
-    public void checkWinnerRow(){
-      // create test for this
-    	TicTacToe t = new TicTacToe();
-		t.mark(1, 0, 0);
-		t.mark(1, 0, 1);
-		t.mark(1, 0, 2);
-		assertTrue("tictactoe should return 'true'", t.checkWin(1));
-    }
+@Test
+public void switchPlayerTest(){
+        // create test for this
+}
 
-    @Test
-    public void checkWinnerColumn(){
-      // create test for this
-    	TicTacToe t = new TicTacToe();
-		t.mark(1, 0, 0);
-		t.mark(1, 1, 0);
-		t.mark(1, 2, 0);
-		assertTrue("tictactoe should return 'true'", t.checkWin(1));
-    }
+@Test
+public void checkWinnerRow(){
+        // create test for this
+        TicTacToe t = new TicTacToe();
+        t.mark(1, 0, 0);
+        t.mark(1, 0, 1);
+        t.mark(1, 0, 2);
+        assertTrue("tictactoe should return 'true'", t.checkWin(1));
+}
 
-    @Test
-    public void checkWinnerColumnPlayer2(){
-      // create test for this
-    	TicTacToe t = new TicTacToe();
-		t.mark(2, 0, 0);
-		t.mark(2, 1, 0);
-		t.mark(2, 2, 0);
-		assertTrue("tictactoe should return 'true'", t.checkWin(2));
-    }
-    @Test
-    public void checkWinnerColumnPlayer2_2(){
-      // create test for this
-    	TicTacToe t = new TicTacToe();
-		t.mark(2, 0, 0);
-		t.mark(2, 1, 0);
-		t.mark(2, 2, 0);
-		assertFalse("tictactoe should return 'false'", t.checkWin(1));
-    }
+@Test
+public void checkWinnerColumn(){
+        // create test for this
+        TicTacToe t = new TicTacToe();
+        t.mark(1, 0, 0);
+        t.mark(1, 1, 0);
+        t.mark(1, 2, 0);
+        assertTrue("tictactoe should return 'true'", t.checkWin(1));
+}
 
-    @Test
-    public void checkWinnerCross(){
-      // create test for this
-    	TicTacToe t = new TicTacToe();
-		t.mark(1, 0, 0);
-		t.mark(1, 1, 1);
-		t.mark(1, 2, 2);
-		assertTrue("tictactoe should return 'true'", t.checkWin(1));
-    }
+@Test
+public void checkWinnerColumnPlayer2(){
+        // create test for this
+        TicTacToe t = new TicTacToe();
+        t.mark(2, 0, 0);
+        t.mark(2, 1, 0);
+        t.mark(2, 2, 0);
+        assertTrue("tictactoe should return 'true'", t.checkWin(2));
+}
+@Test
+public void checkWinnerColumnPlayer2_2(){
+        // create test for this
+        TicTacToe t = new TicTacToe();
+        t.mark(2, 0, 0);
+        t.mark(2, 1, 0);
+        t.mark(2, 2, 0);
+        assertFalse("tictactoe should return 'false'", t.checkWin(1));
+}
 
-    // ADD MANY MORE TEST HERE
+@Test
+public void checkWinnerCross(){
+        // create test for this
+        TicTacToe t = new TicTacToe();
+        t.mark(1, 0, 0);
+        t.mark(1, 1, 1);
+        t.mark(1, 2, 2);
+        assertTrue("tictactoe should return 'true'", t.checkWin(1));
+}
+
+// ADD MANY MORE TEST HERE
 }
