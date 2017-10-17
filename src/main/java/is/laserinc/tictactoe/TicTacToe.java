2d673c344ebe8e9cc38ccb8c4346cb8d90c5d0b3
Ragnarstefanss <ragnars15@ru.is>
2017-10-17 20:39:29 +0000

    Fillboard and PrintBoard coded

--- src/main/java/is/laserinc/tictactoe/TicTacToe.java
+++ src/main/java/is/laserinc/tictactoe/TicTacToe.java
@@ -5,9 +5,36 @@ public class TicTacToe {
 	private int board[][];
 	private boolean winReturn = false;
 	char player;
+	int board_size = 3;
 
 	public TicTacToe() {
-		board = new int[3][3];
+		board = new int[board_size][board_size];
+	}
+
+	public void ChangePlayer(char player_type) {
+		player = player_type;
+	}
+
+	public void PrintPlayer() {
+		System.out.println("Player " + player + " turn: ");
+	}
+
+	public void FillBoard() {
+		int counter = 0;
+		for(int i = 0; i < board_size; i++) {
+			for(int j = 0; j < board_size; j++) {
+				board[i][j] = counter;
+				counter++;
+			}
+		}
+	}
+
+	public void PrintBoard() {
+		System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2] );
+		System.out.println("---------");
+		System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2] );
+		System.out.println("---------");
+		System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2] );
 	}
 
 	public void mark(int player, int x, int y) {
@@ -47,17 +74,14 @@ public class TicTacToe {
 		}
 		return winReturn;
 	}
-	public void ChangePlayer(char player_type) {
-		player = player_type;
-	}
 
-	public void PrintPlayer(){
-		System.out.println("Player " + player + " turn: ");
-	}
 
 	public static void main(String[] args) {
     // CODE HERE
 		TicTacToe Game = new TicTacToe();
+		Game.FillBoard();
+		Game.PrintBoard();
+
 		Game.ChangePlayer('X');
 		Game.PrintPlayer();
 
