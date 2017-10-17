6040f7a1bd7b4de2354e7e199cd3adc81bf09160
Ragnarstefanss <ragnars15@ru.is>
2017-10-17 20:00:25 +0000

    Validation.java beutified

--- src/main/java/is/laserinc/tictactoe/validation.java
+++ src/main/java/is/laserinc/tictactoe/validation.java
@@ -1,43 +1,36 @@
 bool verification(string name){
-	
-	Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-	Matcher m = p.matcher(name);
-	boolean b = m.find();
- 
-	if(name.length() > 12 || name.length() < 1){
-		
-		return false;
-	}
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(name);
+		boolean b = m.find();
 
-	else if (b){
+		if(name.length() > 12 || name.length() < 1) {
+			return false;
+		}
+		else if (b) {
+			return false;
+		}
 
-		return false;
-	}
-
-	else{
-		return true;
-	}
+		else {
+			return true;
+		}
 }
 
 @Test
-	public void nameOk(){
-		assertEquals(TRUE, TicTacToe.verification("H4FD15"));
-	}
+public void nameOk(){
+	assertEquals(TRUE, TicTacToe.verification("H4FD15"));
+}
 
 @Test
-	public void specialChar(){
-		assertEquals(FALSE, TicTacToe.verification("H4fd1?"));
-	}
+public void specialChar(){
+	assertEquals(FALSE, TicTacToe.verification("H4fd1?"));
+}
 
 @Test
-	public void nameToLong(){
-		assertEquals(FALSE, TicTacToe.verification("H4fd15543L4ND1337N3RD"));
-	}
+public void nameToLong(){
+	assertEquals(FALSE, TicTacToe.verification("H4fd15543L4ND1337N3RD"));
+}
 
 @Test
-	public void emptyName(){
-		assertEquals(FALSE, TicTacToe.verification(""));
-	}
-
-
-
+public void emptyName(){
+	assertEquals(FALSE, TicTacToe.verification(""));
+}
