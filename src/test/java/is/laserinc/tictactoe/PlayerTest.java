26d127c2a09a9f1935950469206cdbb4e32771ca
ragnarstefanss <ragnarstefanss@gmail.com>
2017-10-25 11:26:10 +0000

    ChangePlayer test DONE

--- src/test/java/is/laserinc/tictactoe/PlayerTest.java
+++ src/test/java/is/laserinc/tictactoe/PlayerTest.java
@@ -5,5 +5,11 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 	// TESTS FOR FUNCTION: changeToPlayer(char changeTo)
+  @Test
+  public void checkChangePlayer(){
+    Player player = new Player('x');
+    player.changeToPlayer('o');
+    assertEquals('o', player.currentPlayer());
+  }
 
 }
