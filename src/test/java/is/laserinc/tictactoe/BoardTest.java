b029b5209c04c70937bf4e66168b8bcddd123e1d
unknown <thorirv15@ru.is>
2017-10-25 10:19:06 +0000

    test fyrir checkWin foll

--- src/test/java/is/laserinc/tictactoe/BoardTest.java
+++ src/test/java/is/laserinc/tictactoe/BoardTest.java
@@ -7,7 +7,35 @@ public class BoardTest {
     // TESTS FOR FUNCTION: initializeBoard() 
     // TESTS FOR FUNCTION: canMove()
     // TESTS FOR FUNCTION: mark(char player, int x) 
+    
     // TESTS FOR FUNCTION: checkWin()
+    @Test
+    public void checkWinForX(){
+        Board t = new Board();
+        t.mark('x', 1);
+        t.mark('x', 2);
+        t.mark('x', 3); 
+        assertEquals('x', t.checkWin());   
+    }
+
+    @Test
+    public void checkWinForO(){
+        Board t = new Board();
+        t.mark('o', 1);
+        t.mark('o', 4);
+        t.mark('o', 7); 
+        assertEquals('o', t.checkWin());   
+    }
+
+    @Test
+    public void checkIfNoWinner(){
+        Board t = new Board();
+        t.mark('x', 1);
+        t.mark('o', 4);
+        t.mark('o', 7); 
+        assertEquals('u', t.checkWin());   
+    }
+
 
     // TESTS FOR FUNCTION: CheckWin(char player) 
     @Test
@@ -20,7 +48,7 @@ public class BoardTest {
     }
 
     @Test
-    public void checkWinnerColumn(){
+    public void checkWinnerColumnForO(){
     	Board t = new Board();
     	t.mark('o', 1);
     	t.mark('o', 4);
@@ -29,7 +57,7 @@ public class BoardTest {
     }
 
     @Test
-    public void checkWinnerColumnPlayer2(){
+    public void checkWinnerColumnForX(){
     	Board t = new Board();
     	t.mark('x', 1);
     	t.mark('x', 4);
@@ -38,7 +66,7 @@ public class BoardTest {
     }
 
     @Test
-    public void checkWinnerColumnPlayer2_2(){
+    public void checkWinnerColumnForO2(){
     	Board t = new Board();
     	t.mark('x', 1);
     	t.mark('x', 4);
@@ -53,6 +81,27 @@ public class BoardTest {
     	t.mark('o', 5);
     	t.mark('o', 9);
     	assertTrue("board should return 'true'", t.checkWin('o'));
+    }
+
+    @Test
+    public void checkIfNoWinnerWithFullBoard(){
+        Board t = new Board();
+        t.mark('o', 1);
+        t.mark('x', 2);
+        t.mark('o', 3);
+        t.mark('x', 4);
+        t.mark('o', 5);
+        t.mark('x', 6);
+        t.mark('x', 7);
+        t.mark('o', 8);
+        t.mark('x', 9);
+        assertFalse("board should return 'false'", t.checkWin('o'));
+    }
+
+    @Test
+    public void checkIfNoWinnerWithEmptyBoard(){
+        Board t = new Board();
+        assertFalse("board should return 'false'", t.checkWin('x'));
     }
 
     // TESTS FOR FUNCTION: printBoard()
